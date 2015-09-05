c72a2b2309f8a1e1aff212d4b2ea14168bd4cd48
NWChen <neilwchen@gmail.com>
2015-09-05 06:42:28 -0400

    revert to angelhacks version

--- client/client.js
+++ client/client.js
@@ -1,3 +1,6 @@
 Meteor.startup(function() {
 	GoogleMaps.load();
+	Meteor.call("getPage", "https://en.wikipedia.org/w/api.php?action=query&titles=Empire%20State%20Building&prop=revisions&rvprop=content&format=json", function(error, results) {
+		console.log(results.content);
+	});
 });
