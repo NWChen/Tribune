938c7d9409fee66251f0211d1e8a4c00567055e7
NWChen <neilwchen@gmail.com>
2015-09-05 09:18:18 -0400

    summary generation from Wikipedia API

--- client/client.js
+++ client/client.js
@@ -20,8 +20,11 @@ Meteor.startup(function() {
 	};
 
 	google.maps.event.addDomListener(window, 'load', initialize);
-
-	Meteor.call("getPage", "https://en.wikipedia.org/w/api.php?action=query&titles=Empire%20State%20Building&prop=revisions&rvprop=content&format=json", function(error, results) {
-		console.log(results.content);
+	
+	//obtain and clean up label summary
+	Meteor.call("getPage", "https://en.wikipedia.org/w/api.php?action=query&titles=Chrysler%20Building&prop=extracts&rvprop=content&format=json", function(error, results) {
+		var str = JSON.stringify(results.content.replace(/(<([^>]+)>)/ig,""));
+		var extract = str.substring(str.indexOf("extract")+12, str.indexOf("\\\\n")).replace("\\\\u00a0", " ");
+		console.log(extract);
 	});
 });
